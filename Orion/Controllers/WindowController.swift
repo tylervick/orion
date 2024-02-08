7da5858ff5acd022614a040a62c6d2a74ae29152
Tyler Vick <tyler@tylervick.com>
2024-02-08 12:27:44 -0700

    toolbar storyboard

--- Orion/Controllers/WindowController.swift
+++ Orion/Controllers/WindowController.swift
@@ -8,100 +8,40 @@
 import Cocoa
 
 final class WindowController: NSWindowController {
+    let toolbarViewModel = ToolbarViewModel()
+
+    @IBOutlet var toolbar: Toolbar!
+    @IBOutlet var back: NSToolbarItem!
+    @IBOutlet var forward: NSToolbarItem!
+    @IBOutlet var reload: NSToolbarItem!
+    @IBOutlet var location: NSToolbarItem!
+
     weak var toolbarActionDelegate: ToolbarActionDelegate?
 
-    let vm = ToolbarViewModel()
-
     override func windowDidLoad() {
-        let toolbar = Toolbar(delegate: self)
-//        let toolbar = NSToolbar(identifier: "default")
-//        toolbar.delegate = self
-        window?.toolbar = toolbar
         if let webViewController = contentViewController as? ViewController {
             toolbarActionDelegate = webViewController
         }
 
-//        let tb = NSToolbar(identifier: "toolbar")
-//        tb.delegate = self
-//        window?.toolbar = tb
+        window?.title = ""
     }
 
-    @objc func prevButtonClicked(_: NSToolbarItem) {
+    @IBAction func backButtonClicked(_: NSToolbarItem) {
         print("Prev button clicked")
     }
 
-    @objc func nextButtonClicked(_: NSToolbarItem) {
+    @IBAction func forwardButtonClicked(_: NSToolbarItem) {
         print("Next button clicked")
     }
 
-    @objc func reloadButtonClicked(_: NSToolbarItem) {
+    @IBAction func reloadButtonClicked(_: NSToolbarItem) {
         print("Reload button clicked")
         toolbarActionDelegate?.performReload()
     }
-}
 
-extension WindowController: NSToolbarDelegate {
-    func toolbar(
-        _: NSToolbar,
-        itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier,
-        willBeInsertedIntoToolbar _: Bool
-    ) -> NSToolbarItem? {
-        switch itemIdentifier {
-        case .navGroup:
-            let prevItem = ToolbarItemFactory.createItem(
-                itemIdentifier: .prev,
-                label: "Back",
-                paletteLabel: "Back",
-                toolTip: "Go Back",
-                image: NSImage(
-                    systemSymbolName: "chevron.left",
-                    accessibilityDescription: "A left arrow"
-                ),
-                target: self,
-                action: #selector(prevButtonClicked(_:))
-            )
-            let nextItem = ToolbarItemFactory.createItem(
-                itemIdentifier: .next,
-                label: "Forward",
-                paletteLabel: "Forward",
-                toolTip: "Go forward",
-                image: NSImage(
-                    systemSymbolName: "chevron.right",
-                    accessibilityDescription: "A right arrow"
-                ),
-                target: self,
-                action: #selector(nextButtonClicked(_:))
-            )
-            return ToolbarItemFactory.createItemGroup(
-                itemIdentifier: itemIdentifier,
-                label: nil,
-                paletteLabel: "Navigation",
-                toolTip: "Navigation",
-                items: [prevItem, nextItem]
-            )
-        case .reload:
-            return ToolbarItemFactory.createItem(
-                itemIdentifier: .reload,
-                label: "Reload",
-                paletteLabel: "Reload",
-                toolTip: "Reload Page",
-                image: NSImage(
-                    systemSymbolName: "arrow.clockwise",
-                    accessibilityDescription: "A clockwise arrow"
-                ),
-                target: self,
-                action: #selector(reloadButtonClicked(_:))
-            )
-        default:
-            return nil
-        }
-    }
-
-    func toolbarAllowedItemIdentifiers(_: NSToolbar) -> [NSToolbarItem.Identifier] {
-        [.navGroup]
-    }
-
-    func toolbarDefaultItemIdentifiers(_: NSToolbar) -> [NSToolbarItem.Identifier] {
-        [.navGroup]
+    @IBAction func locationSent(_: Any) {
+        print("location sent")
     }
 }
+
+extension WindowController: NSToolbarDelegate {}
--- Orion/Views/Toolbar/Toolbar.swift
+++ Orion/Views/Toolbar/Toolbar.swift
@@ -7,57 +7,11 @@
 
 import AppKit
 
+// TODO: Probably delete this if it's not doing anything useful
 final class Toolbar: NSToolbar {
-    
-    init(identifier: NSToolbar.Identifier = "toolbar", delegate: NSToolbarDelegate) {
-        super.init(identifier: identifier)
-        
-        self.delegate = delegate
-    }
-}
-
-extension NSToolbarItem.Identifier {
-    static let prev = NSToolbarItem.Identifier(rawValue: "prev")
-    static let next = NSToolbarItem.Identifier(rawValue: "next")
-    static let navGroup = NSToolbarItem.Identifier(rawValue: "navGroup")
-
-    static let reload = NSToolbarItem.Identifier(rawValue: "reload")
-}
-
-enum ToolbarItemFactory {
-    static func createItem(
-        itemIdentifier: NSToolbarItem.Identifier,
-        label: String,
-        paletteLabel: String,
-        toolTip: String,
-        image: NSImage?,
-        target: AnyObject?,
-        action: Selector?
-    ) -> NSToolbarItem {
-        let item = NSToolbarItem(itemIdentifier: itemIdentifier)
-        item.label = label
-        item.paletteLabel = paletteLabel
-        item.toolTip = toolTip
-        item.image = image
-        item.target = target
-        item.action = action
-        return item
-    }
-
-    static func createItemGroup(
-        itemIdentifier: NSToolbarItem.Identifier,
-        label: String?,
-        paletteLabel: String,
-        toolTip: String,
-        items: [NSToolbarItem]
-    ) -> NSToolbarItemGroup {
-        let itemGroup = NSToolbarItemGroup(itemIdentifier: itemIdentifier)
-        if let label {
-            itemGroup.label = label
-        }
-        itemGroup.paletteLabel = paletteLabel
-        itemGroup.toolTip = toolTip
-        itemGroup.subitems = items
-        return itemGroup
-    }
+//    init(identifier: NSToolbar.Identifier = "toolbar", delegate: NSToolbarDelegate) {
+//        super.init(identifier: identifier)
+//
+//        self.delegate = delegate
+//    }
 }
